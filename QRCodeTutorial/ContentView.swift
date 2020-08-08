eb1e861edfceea3d16c24551df53601c76dcaf6a
Gavin kim <dreamworld612@gmail.com>
2020-08-08 20:44:00 +0900

    Initial Commit

---
+++ QRCodeTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QRCodeTutorial
+//
+//  Created by Gavin on 2020/08/08.
+//  Copyright © 2020 Gavin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
